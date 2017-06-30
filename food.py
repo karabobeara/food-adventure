780f1a6cc6b5db4fb87462c5b8e16ad3823a0021
CrystalM <crystalm@Macs-MacBook-Pro-99.local>
2017-06-30 14:25:55 -0700

    add food.py

--- food.py
+++ food.py
@@ -1,7 +1,8 @@
-#<<<<<<< HEAD
+
 
 
 #lets go!!!
+
 
 
 def replay():
@@ -18,7 +19,9 @@ def game():
     couch = input("Get off the couch? (yes or no)\n")
     if couch == "yes":
 #tab here
+
         print ("* walk to kitchen *")
+        time.sleep(1)
 #start 2nd path
             # in the kitchen
         choice2 = input("Do I go to the fridge, freezer, or Costco?")
@@ -26,44 +29,137 @@ def game():
             # picked fridge
         if choice2 == "fridge":
             print ("You have chosen the fridge.  There are yummy food.")
-            time.sleep(2)
+            time.sleep(1)
             print ("...")
-            time.sleep(2)
+            time.sleep(1)
             print ("walking")
-            time.sleep(2)
+            time.sleep(1)
             print ("... destination reached.")
             time.sleep(1)
-            choice5 = input("Do you chose the salad or leftovers?")
-
-            # make person pick salad or left overs
+            choice2a = input("Do you chose the salad or leftovers?")
 
 
 
-            # picked freezer
-        if choice2 == "freezer":
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            # pick salad
+        if choice2a == "freezer":
             print ("You have chosen the colder fridge.  There is ice cream and hot pockets. Well, cold right now.")
-            time.sleep(2)
+            time.sleep(1)
             print ("...")
-            time.sleep(2)
+            time.sleep(1)
             print ("walking")
-            time.sleep(2)
+            time.sleep(1)
             print ("... destination reached.")
             time.sleep(1)
             print ("Do which one do you pick?")
 
             # make them chose one & branch out
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             # picked Costco
         if choice2 == "Costco":
-            time.sleep(2)
+            time.sleep(1)
             print ("...")
-            time.sleep(2)
+            time.sleep(1)
             print ("walking")
-            time.sleep(2)
+            time.sleep(1)
             print ("... destination reached.")
             time(1)
-            print ("Lol. Why, there's food in your fridge. What do you want to waste money (protien bars) or save money (free samples)?")
+            print ("Lol. Why, there's food in your fridge.")
+
+            choicec = input("What do you want to waste money (protien bars) or save money (free samples)?")
+            if choicec == "protien bars":
+                print ("Now, I have less money and have to workout.")
+
+
+            if choicec == "free samples":
+                print (".")
             #food pick
+
+
+
+
+
+
+
+
+
+
     else:
         print ("You stayed on the couch for 3 days and starved.")
         replay()
