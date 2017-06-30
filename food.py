fb3b6dfdb90bb45e6d0b21c152f0e4c5f1363474
CrystalM <crystalm@Macs-MacBook-Pro-99.local>
2017-06-30 13:28:13 -0700

    add food.py

--- food.py
+++ food.py
@@ -1,1 +1,93 @@
-se
+
+import time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# in the kitchen
+print ("Do I go to the fridge, freezer, or Costco?")
+choice5 = raw_input ()
+
+# picked fridge
+if choice5 in ['fridge']:
+    print ("You have chosen the fridge.  There are yummy food.")
+    time.sleep(2)
+    print ("...")
+    time.sleep(2)
+    print ("walking")
+    time.sleep(2)
+    print ("... destination reached.")
+    time.sleep(1)
+    print ("Do you chose the salad or leftovers?")
+
+
+
+# picked freezer
+if choice5 in ['freezer']:
+    print ("You have chosen the colder fridge.  There is ice cream and hot pockets. Well, cold right now.")
+    time.sleep(2)
+    print ("...")
+    time.sleep(2)
+    print ("walking")
+    time.sleep(2)
+    print ("... destination reached.")
+    time.sleep(1)
+    print ("Do which one do you pick?")
+
+# picked Costco
+if choice5 in ['Costco']:
+    time.sleep(2)
+    print ("...")
+    time.sleep(2)
+    print ("walking")
+    time.sleep(2)
+    print ("... destination reached.")
+    time(1)
+    print ("Lol. Why, there's food in your fridge. What do you want to waste money (protien bars) or save money (free samples)?")
