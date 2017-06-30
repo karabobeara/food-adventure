83b9e80680ba419a65e040d7bcdf3e62eec0024c
CrystalM <crystalm@Macs-MacBook-Pro-99.local>
2017-06-30 13:53:48 -0700

    add food.py

--- food.py
+++ food.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 
 
 #lets go!!!
@@ -20,103 +20,54 @@ def game():
 #tab here
         print ("* walk to kitchen *")
 #start 2nd path
+            # in the kitchen
+        choice2 = input("Do I go to the fridge, freezer, or Costco?")
+
+            # picked fridge
+        if choice2 == "fridge":
+            print ("You have chosen the fridge.  There are yummy food.")
+            time.sleep(2)
+            print ("...")
+            time.sleep(2)
+            print ("walking")
+            time.sleep(2)
+            print ("... destination reached.")
+            time.sleep(1)
+            choice5 = input("Do you chose the salad or leftovers?")
+
+            # make person pick salad or left overs
+
+
+
+            # picked freezer
+        if choice2 == "freezer":
+            print ("You have chosen the colder fridge.  There is ice cream and hot pockets. Well, cold right now.")
+            time.sleep(2)
+            print ("...")
+            time.sleep(2)
+            print ("walking")
+            time.sleep(2)
+            print ("... destination reached.")
+            time.sleep(1)
+            print ("Do which one do you pick?")
+
+            # make them chose one & branch out
+
+            # picked Costco
+        if choice2 == "Costco":
+            time.sleep(2)
+            print ("...")
+            time.sleep(2)
+            print ("walking")
+            time.sleep(2)
+            print ("... destination reached.")
+            time(1)
+            print ("Lol. Why, there's food in your fridge. What do you want to waste money (protien bars) or save money (free samples)?")
+            #food pick
     else:
         print ("You stayed on the couch for 3 days and starved.")
         replay()
 
 game()
-=======
 
-import time
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# in the kitchen
-print ("Do I go to the fridge, freezer, or Costco?")
-choice5 = raw_input ()
-
-# picked fridge
-if choice5 in ['fridge']:
-    print ("You have chosen the fridge.  There are yummy food.")
-    time.sleep(2)
-    print ("...")
-    time.sleep(2)
-    print ("walking")
-    time.sleep(2)
-    print ("... destination reached.")
-    time.sleep(1)
-    print ("Do you chose the salad or leftovers?")
-
-
-
-# picked freezer
-if choice5 in ['freezer']:
-    print ("You have chosen the colder fridge.  There is ice cream and hot pockets. Well, cold right now.")
-    time.sleep(2)
-    print ("...")
-    time.sleep(2)
-    print ("walking")
-    time.sleep(2)
-    print ("... destination reached.")
-    time.sleep(1)
-    print ("Do which one do you pick?")
-
-# picked Costco
-if choice5 in ['Costco']:
-    time.sleep(2)
-    print ("...")
-    time.sleep(2)
-    print ("walking")
-    time.sleep(2)
-    print ("... destination reached.")
-    time(1)
-    print ("Lol. Why, there's food in your fridge. What do you want to waste money (protien bars) or save money (free samples)?")
->>>>>>> fb3b6dfdb90bb45e6d0b21c152f0e4c5f1363474
+#>>>>>>> fb3b6dfdb90bb45e6d0b21c152f0e4c5f1363474
