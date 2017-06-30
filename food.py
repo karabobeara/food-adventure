17a37672cd1aaa5a54dffd827ccd481da6f709b2
KaraL <karal@Macs-MacBook-Pro-99.local>
2017-06-30 15:45:12 -0700

    add food.py

--- food.py
+++ food.py
@@ -7,7 +7,7 @@ def replay():
     if ask == "yes":
         game()
     else:
-        print ("Well fine then....\n")
+        print ("Well fine then....you suck.\n")
 
 def game():
 
@@ -21,75 +21,94 @@ def game():
         time.sleep(1)
 #start 2nd path
             # in the kitchen
-        choice2 = input("Do I go to the fridge, freezer, or Costco?")
+        choice2 = input("Do I go to the fridge, freezer, or costco?\n")
 
             # picked fridge
         if choice2 == "fridge":
-            print ("You have chosen the fridge.  There are yummy food.")
+            print ("You have chosen the fridge.  There is yummy food.")
             time.sleep(1)
             print ("...")
             time.sleep(1)
             print ("walking")
             time.sleep(1)
-            print ("... destination reached.")
+            print ("... destination reached.\n")
             time.sleep(1)
             choice2a = input("Do you chose the salad or leftovers?")
 
             # pick salad
             if choice2a == "salad":
-                print ("Congrats! You're healthy....must be nice...")
+                print ("Congrats! You're healthy...\n")
+                time.sleep(1)
+                print ("for once...\nmust be nice...")
+                time.sleep(1)
+                print ("lol. You win at life. jk only this game.\n")
                 replay()
             # pick leftovers
             if choice2a == "leftovers":
-                print ("Did you even check them? No, of course you didn't.... you got dysentary")
+                print ("Did you even check them? No, of course you didn't...you got dysentary\n")
                 replay()
 
         if choice2 == "freezer":
-            print ("You have chosen the colder fridge.  There are ice cream and hot pockets. Well, cold right now.")
+            print ("You have chosen the colder fridge.\nThere are ice cream and hot pockets. Well, cold right now.")
             time.sleep(2)
             print ("...")
             time.sleep(1)
             print ("* walking *")
             time.sleep(1)
-            print ("... destination reached.")
+            print ("... destination reached.\n")
             time.sleep(1)
-            choice2b = input("Which do you pick? (ice cream or hot pocket)")
+            choice2b = input("Which do you pick? (ice cream or hot pocket)\n")
 
             #pick ice cream
             if choice2b == "ice cream":
-                print ("Now go watch Netflix")
+                print ("Now go watch Netflix.\n")
+                time.sleep(2)
+                print ("...You're now a professonal procrastinator. LOL.\nNow get your butt off the couch and try again.\n")
                 replay()
             #pick hot pocket
             if choice2b == "hot pocket":
-                choice2bi = input("Microwave or thaw?")
-                if choice2bi == "microwave"
-                    print ("You burn your toungue yet the middle is still freezing...delicious, right?")
+                choice2bi = input("Microwave or thaw?\n")
+                if choice2bi == "microwave":
+                    print ("You burn your toungue yet the middle is still freezing...delicious, right?\nYou rush to the ER due to excessive heartburn...\n")
+                    time.sleep(3)
+                    print ("The anticipation is killing me...no wait it's killing you.\n")
+                    time.sleep(2)
+                    print("You died. LOL.\n")
                     replay()
 
-                if choice2bi == "thaw"
-                    print ("You were starving and gnawed the frozen hunk of cheese and bread... delicious, right?")
+                if choice2bi == "thaw":
+                    print ("You were starving and gnawed the frozen hunk of cheese and bread\n...you chipped a molar\n...delicious, right?\n")
                     replay()
 
             # picked Costco
-        if choice2 == "Costco":
+        if choice2 == "costco":
             time.sleep(1)
             print ("...")
             time.sleep(1)
             print ("walking")
             time.sleep(1)
-            print ("... destination reached.")
-            time(1)
-            print ("Lol. Why, there's food in your fridge.")
+            print ("... destination reached.\n")
+            time.sleep(1)
+            print ("Lol. Why, there's food in your fridge.\n")
 
-            choice2c = input("Do you want to waste money (protien bars)? Save money (free samples)?")
-            if choice2c == "protien bars":
-                print ("Now, I have less money and I have to workout :(")
+            choice2c = input("Do you want to waste money (protein bars)? Save money (free samples)?\n")
+            if choice2c == "protein bars":
+                print ("Now you have less money and have to workout :(\n")
+                time.sleep(2)
+                print("You go home...\n")
+                time.sleep(2)
+                print ("You attempt to do a push-up\n...you break both arms. LOL.\n")
+                replay()
 
-            if choicec == "free samples":
-                print ("Yay free food! You must budget...noice.")
-
-
-
+            if choice2c == "free samples":
+                print ("Yay free food! You must budget...noice.\n")
+                time.sleep(1)
+                print ("3 years later...\n")
+                time.sleep(3)
+                print ("You have saved enough for a mansion. LIT.\n")
+                time.sleep(3)
+                print ("But you're alone. LOL.\n")
+                replay()
 
 
 
@@ -99,7 +118,7 @@ def game():
 
 #first ending
     else:
-        print ("You stayed on the couch for 3 days and starved.")
+        print ("You stayed on the couch for 3 days and starved to death. LOL.\n")
         replay()
 
 game()
